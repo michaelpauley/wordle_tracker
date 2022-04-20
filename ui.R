2fdf5280f22ebceca66877459ef1268c5b033c4a
michaelpauley <85527356+michaelpauley@users.noreply.github.com>
2022-04-20 14:40:19 -0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,9 +1,13 @@
 library(shiny)
 
 fluidPage(
-  strong("This app is meant to test AWS"),hr(),
-  selectInput(inputId = "select",label = "Did this page load for you", choices = c("Yes","No")),hr(),
-  textOutput("text")
+  strong("Mike Pauley - Tucson, AZ"),hr(),
+  textInput(inputId = "how",label = "How did you find this page?"),
+  actionButton(inputId = "send", "Submit"),
+  textOutput("text"),hr(),
+  a(actionButton(inputId = "email1", label = "Contact Me", 
+                 icon = icon("envelope", lib = "font-awesome")),
+    href="mailto:michaelrpauley@gmail.com")
   
   
   
