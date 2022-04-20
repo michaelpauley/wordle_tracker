5b41cafca16104b1cd9044d5ad65f73afc8c8966
michaelpauley <85527356+michaelpauley@users.noreply.github.com>
2022-04-20 16:11:20 -0700

    as

--- server.R
+++ server.R
@@ -1,15 +1,24 @@
-server <- function(input, output, session){
+server <- function(input, output) {
   
-  
-  observeEvent(input$send,{
-  output$text <- renderText({
-   
-    "Thank you for the info"
-     
-   })
+  output$letter_plot <- renderPlot({  
+    
+    plot <- ggplot(wordle_tally, aes(x=letter,y=count)) +
+      geom_histogram(stat="identity",fill="dodgerblue3")
+    
+    
+    plot
   })
+  
+  output$download_wordle <- downloadHandler(
+    filename = function() {
+      paste("data-", Sys.Date(), ".csv", sep="")
+    },
+    content = function(file) {
+      write.csv(wordle, file)
+    }
+  )
+  
+  
   
   
 }
-
-
